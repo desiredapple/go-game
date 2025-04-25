5084c529d78fca637e58ec41ed4f986a824a97e7
desiredapple <123669481+desiredapple@users.noreply.github.com>
2025-04-25 09:46:20 +0300

    add enum, fix naming

--- Models/Stone.cs
+++ Models/Stone.cs
@@ -1,9 +1,25 @@
+public enum StoneColor : byte
+{
+    White,
+    Black
+}
+
 namespace Stone
 {
     public class Stone
     {
-        private bool color;
-        private int x;
-        private int y;
+        public Stone()
+        {
+            //Некоторая переменная i, отвечающая за нумерацию ходов. Т. к. черные начинают первые, то
+            //четные ходы делают черные, нечетные — белые
+            //color = i % 2 ? StoneColor.White : StoneColor.Black;
+        }
+        public StoneColor Color
+        {
+            get => _color;
+        }
+        private readonly StoneColor _color;
+        private int _x;
+        private int _y;
     }
 }
