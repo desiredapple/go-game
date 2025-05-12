07003c4957e9cb7c0e75e7e8fbabb57878765427
EntireC <91829056+EntireC@users.noreply.github.com>
2025-05-12 19:13:19 +0300

    boardstate static classes

--- Models/Boardstate.cs
+++ Models/Boardstate.cs
@@ -13,5 +13,31 @@ namespace boardstate
         // храним две последние позиции доски для правила КО
         private int[][] _previousField;
         private int[][] _prePreviousField;
+
+        // копирование доски
+        private static void CopyField(int[][] source, int[][] clone)
+        {
+            for (int i = 0; i < source.Length; i++)
+                Array.Copy(source[i], clone[i], source[i].Length);
+        }
+
+        // создать пустое поле 
+        private static int[][] CreateEmptyField(int size)
+        {
+            var f = new int[size][];
+            for (int i = 0; i < size; i++)
+                f[i] = new int[size];
+            return f;
+        }
+
+        // сравнение двух полей поэлементно
+        private static bool FieldsEqual(int[][] a, int[][] b)
+        {
+            for (int x = 0; x < a.Length; x++)
+                for (int y = 0; y < a.Length; y++)
+                    if (a[x][y] != b[x][y])
+                        return false;
+            return true;
+        }
     }
 }
