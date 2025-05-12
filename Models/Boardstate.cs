8c495da54e9b31355429764bb8821be849b89f2f
EntireC <91829056+EntireC@users.noreply.github.com>
2025-05-12 19:16:24 +0300

    stone placement rulecheck

--- Models/Boardstate.cs
+++ Models/Boardstate.cs
@@ -54,5 +54,43 @@ namespace boardstate
                         return false;
             return true;
         }
+
+        // вызывается перед каждым новым реальным ходом, сохраняем историю
+        private void SaveHistory()
+        {
+            CopyField(_previousField, _prePreviousField);
+            CopyField(_field, _previousField);
+        }
+
+        // при нажатии на пункт, вызывается конструктор камня, а потом PlaceStone с этим камнем и координатами пункта
+        public void PlaceStone(Stone stone, int x, int y)
+        {
+            // проверка чтобы пункт был в границах доски
+            if (x < 0 || x >= _size || y < 0 || y >= _size)
+                throw new ArgumentOutOfRangeException(nameof(x), "Координаты вне доски");
+
+            // проверка чтобы пункт был пустой
+            if (_field[x][y] != 0)
+                throw new InvalidOperationException("Клетка уже занята");
+
+            // проверка правила Ко: симулируем ход и сравниваем с позапрошлой позицией
+            if (WouldViolateKo(x, y, stone.Color))
+                throw new InvalidOperationException("Нарушение правила Ко");
+
+
+            // тут нужна проверка правила суицидального камня
+
+            SaveHistory();
+            
+            // обновляем цвет пункта на доске
+            _field[x][y] = stone.Color == StoneColor.Black ? 1 : 2;
+
+            // меняем координаты на настоящие у камня 
+            stone.SetPosition(x, y);
+
+            // тут нужна функция с проверкой/реализацией сруба
+            // тут нужно обновить нынешнее поле, если сруб проходит, убрать срубленные камни
+
+        }
     }
 }
