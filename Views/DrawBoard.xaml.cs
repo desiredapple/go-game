c640209b6db0503f25f7fbad461c6e49d745852d
desiredapple <123669481+desiredapple@users.noreply.github.com>
2025-05-13 18:33:34 +0300

    add proj struct

--- Views/DrawBoard.xaml.cs
+++ Views/DrawBoard.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Shapes;
 
 namespace Game_go
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private int n = 19;
