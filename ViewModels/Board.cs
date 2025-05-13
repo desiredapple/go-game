a97b59b5b5b0e3cf48368156700e3121e2b3fa12
desiredapple <123669481+desiredapple@users.noreply.github.com>
2025-05-13 22:28:44 +0300

    move boardstate logic into board

--- ViewModels/Board.cs
+++ ViewModels/Board.cs
@@ -14,4 +14,33 @@ public class Board
         _size = size;
         _field = new CellStatus[size, size];
     }
+
+    private void CopyField(CellStatus[,] source, CellStatus[,] clone)
+    {
+        for (int i = 0; i < _size; ++i)
+            Array.Copy(source[i], clone[i], _size);
+    }
+
+    private CellStatus[,] CreateEmptyField(int size)
+    {
+        var f = new int[size][];
+        for (int i = 0; i < size; ++i)
+            f[i] = new int[size];
+        return f;
+    }
+
+    private bool FieldsEqual(CellStatus[,] a, CellStatus[,] b)
+    {
+        for (int x = 0; x < _size; ++x)
+            for (int y = 0; y < _size; ++y)
+                if (a[x][y] != b[x][y])
+                    return false;
+        return true;
+    }
+
+    private void SaveHistory()
+    {
+        CopyField(_previousField, _prePreviousField);
+        CopyField(_field, _previousField);
+    }
 }
