a644d9e3b69af33dc7d1c8cd5846f101f66d4e0b
e <maximkibardin2005@gmail.com>
2025-05-13 03:50:13 +0300

    add GetSize method

--- Models/BoardSizeExtensions.cs
+++ Models/BoardSizeExtensions.cs
@@ -1,14 +1,18 @@
-﻿using System;
-
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
+﻿
+namespace BoardSizeExtensions
 {
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+    public static class BoardSizeExtensions
+    {
+        public static int GetSize(this BoardSize size)
+        {
+            return size switch
+            {
+                BoardSize.Ultrasmall => 5,
+                BoardSize.Small => 7,
+                BoardSize.Medium => 15,
+                BoardSize.Default => 19,
+                _ => throw new ArgumentOutOfRangeException(nameof(size), $"Unknown board size: {size}")
+            };
+        }
+    }
 }
