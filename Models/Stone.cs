4b444d3ffc7719f578de3dedd57e71b7b31ee0f0
desiredapple <123669481+desiredapple@users.noreply.github.com>
2025-05-14 22:16:01 +0300

    fix interface

--- Models/Stone.cs
+++ Models/Stone.cs
@@ -3,26 +3,11 @@ namespace GoGame.Models;
 public class Stone
 {
     private readonly CellStatus _status;
-    private readonly int _x;
-    private readonly int _y;
 
     public CellStatus Status { get { return _status; } }
 
-    public Stone(CellStatus status, int x, int y)
+    public Stone(CellStatus status)
     {
-        _x = x;
-        _y = y;
         _status = status;
-
-            }
-            public int Y
-            {
-                get => _x;
-            }
-            
-      
-
-            private readonly StoneColor _color;
-            private int _x;
-            private int _y;
+    }
 }
